f8078f96e9b27ac319d638c70bf6d97eee143e6b
michael <ittybemain@gmail.com>
2021-05-07 14:02:05 +0200

    borrow some code for markdown styling

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,19 +1,19 @@
 @extends("layouts\mainlayout")
 
 @section("title")
-    $post["postname"]
+    {{$post["postname"]}}
 @endsection
 
 @section("content")
 <div class="section">
-    <div href={{"post/" . strval($post["id"])}} class="post section tile">
+    <div class="post section tile">
         <div>{{$post["postname"]}}</div>
         <div>created at {{$post["created_at"]}}</div>
         @if (!empty($post["updated_at"]))
             <div>updated at {{$post["updated_at"]}}</div>
         @endif
-        <div class="post-body">
-        {!!$markdown!!}
+        <div class="markdown-body">
+            {!!$markdown!!}
         </div>
     </div>
 </div>
